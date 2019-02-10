a7adbc31e5d264443f2c7add167f7c41c85db88a
Aditya Kamble <adityakamble49@gmail.com>
2019-02-10 19:38:12 +0530

    Logistic Regression working for predefined theta

--- supervised/classification/logisticregression/logistic_regression.py
+++ supervised/classification/logisticregression/logistic_regression.py
@@ -1,10 +1,10 @@
 # # Logistic Regression using Gradient Descent
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-
 # ## Import Data Set
+from sklearn.preprocessing import PolynomialFeatures
 
 train_data = pd.read_csv('micro_data_train.csv')
 
@@ -24,31 +24,22 @@ plt.scatter(train_data_0.iloc[:, 0], train_data_0.iloc[:, 1], marker='o', color=
 plt.xlabel('Micro Test 1')
 plt.ylabel('Micro Test 2')
 plt.legend(labels=['Accepted', 'Rejected'])
-plt.show()
+# plt.show()
 
 # ## Feature Separation and Normalization
 
 X_train = train_data.iloc[:, :-1].values
-y_train = train_data.iloc[:, -1:].values
+y_train = train_data.iloc[:, -1].values
 m = y_train.shape[0]
 
 X_test = test_data.iloc[:, :].values
 
+poly = PolynomialFeatures(6)
+X_train_mapped = poly.fit_transform(X_train[:, 0:2])
+X_test_mapped = poly.fit_transform(X_test[:, -1:])
 
-def map_features(x1, x2):
-    degree = 6
-    mapped_result = np.ones((x1.shape[0], sum(range(degree + 2))))
-    current_col = 1
-    for i in range(1, degree + 1):
-        for j in range(i + 1):
-            mapped_result[:, current_col] = np.power(x1, i - j)
-            np.power(x2, j)
-            current_col += 1
-    return mapped_result
-
-
-X_train_mapped = map_features(X_train[:, 0], X_train[:, 1])
-X_test_mapped = map_features(X_test[:, 0], X_test[:, 1])
+cost_history = []
+gradient_history = []
 
 
 # ## Gradient Descent
@@ -58,28 +49,57 @@ def sigmoid(input_var):
     return sigmoid_result
 
 
-def cost_grad_reg(X, y, theta, lambda_val):
-    z = np.matmul(X, theta)
+def compute_cost_reg(theta, X, y):
+    z = np.dot(X, theta)
     hx = sigmoid(z)
     neg_0_cost = (-1 * (1 - y)) * np.log((1 - hx))
     pos_1_cost = (-1 * y) * np.log(hx)
     cost_normal = (pos_1_cost + neg_0_cost) / m
-    cost_reg = (lambda_val / (2 * m)) * np.sum(np.power(theta[1, :], 2))
+    cost_reg = (lambda_value / (2 * m)) * np.sum(np.power(theta[1:], 2))
     cost_normal_sum = np.sum(cost_normal)
     cost = cost_normal_sum + cost_reg
+    cost_history.append(cost)
+    return cost.flatten()
 
+
+def gradient_reg(theta, X, y):
+    z = np.dot(X, theta)
+    hx = sigmoid(z)
     error_value = hx - y
     error_value_final = np.matmul(error_value.T, X)
     delta = error_value_final / m
-    delta_reg = (lambda_val / m) * theta.T
+    delta_reg = (lambda_value / m) * theta.reshape(-1, 1).T
     delta_reg[0] = 0
     delta_total = delta + delta_reg
-    return [cost, delta_total]
+    grad = delta_total.flatten()
+    gradient_history.append(gradient_history)
+    return grad.flatten()
 
 
-alpha = 0.1
 lambda_value = 1
-theta_value = np.zeros((X_train_mapped.shape[1], 1))
-[cost, delta_total_result] = cost_grad_reg(X_train_mapped, y_train, theta_value, lambda_value)
-print(cost)
-print(delta_total_result)
+theta_value = np.zeros(X_train_mapped.shape[1])
+cost = compute_cost_reg(theta_value, X_train_mapped, y_train)
+grad = gradient_reg(theta_value, X_train_mapped, y_train)
+
+print("For lambda = 1 and theta = zeros")
+print("Cost: " + str(cost))
+print("Grad (First 5): " + str(grad[:5]))
+
+lambda_value = 10
+theta_value = np.ones(X_train_mapped.shape[1])
+cost = compute_cost_reg(theta_value, X_train_mapped, y_train)
+grad = gradient_reg(theta_value, X_train_mapped, y_train)
+
+print("For lambda = 10 and theta = ones")
+print("Cost: " + str(cost))
+print("Grad (First 5): " + str(grad[:5]))
+
+# Rest history
+cost_history.clear()
+gradient_history.clear()
+
+# lambda_value = 1
+# theta_value = np.zeros(X_train_mapped.shape[1])
+# result = optimize.minimize(fun=compute_cost_reg, x0=theta_value, args=(X_train_mapped, y_train), method='TNC',
+#                            jac=gradient_reg)
+# print(result)
